1f9811191d75c82978542942ac76f0c4cb690356
hosamhany <7ossHany@gmail.com>
2017-09-09 17:24:43 +0200

    created the login and register request and linked with backend

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,44 @@
-angular.module("app", ['ui.ace'])
-    .controller("controller", ["$scope", function($scope) {
+var app = angular.module("app", ['ui.ace'])
+    .controller("controller", ["$scope","$window","loginService", function($scope,$window,loginService) {
         $scope.code = "default code";
         $scope.output= "output";
+        $scope.login= function()
+        {
+          $window.location.href = 'http://192.168.1.12:3000/login';
+        };
+        $scope.register= function()
+        {
+          $window.location.href = 'http://192.168.1.12:3000/register';
+        };
     }]);
+
+    app.service('loginService',['$http', function($http) {
+      // delete $http.defaults.headers.common['X-Requested-With'];
+      this.getData = function() {
+        // $http() returns a $promise that we can add handlers with .then()
+          return $http({
+            method: 'GET',
+            url: 'http://192.168.1.12:3000/login',
+            params: '',
+            headers: {}
+         });
+     };
+}]);
+app.service('registerService',['$http', function($http) {
+  delete $http.defaults.headers.common['X-Requested-With'];
+  this.getData = function() {
+    // $http() returns a $promise that we can add handlers with .then()
+      return $http({
+        method: 'GET',
+        url: 'http://192.168.1.12:3000/register',
+        params: '',
+        headers: {}
+     }).success(function (data){
+       console.log(data);
+     })
+     .error(function(data)
+   {
+    //  console.log(data);
+   });
+ };
+}]);
